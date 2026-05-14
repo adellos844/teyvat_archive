c1c489e877a26f01295e6cb5524fe4099e8255f8
adellos844 <adellos844@g.educaand.es>
2026-05-14 14:03:23 +0200

    Decimocuarto commit del proyecto. Subida de la página y creación del superuser

--- core/settings.py
+++ core/settings.py
@@ -127,3 +127,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+ALLOWED_HOSTS = ['teyvat-archive.onrender.com', 'localhost', '127.0.0.1']
